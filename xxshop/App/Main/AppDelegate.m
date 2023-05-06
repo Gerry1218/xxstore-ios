4623b08708fb4a6f3e434f6493454d742144634a
gerry <gerry1218@163.com>
2023-05-06 13:53:08 +0800

    调用localhost的rn页面并展示

--- xxshop/App/Main/AppDelegate.m
+++ xxshop/App/Main/AppDelegate.m
@@ -20,7 +20,6 @@
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     TestViewController *vc = [[TestViewController alloc] init];
-    vc.view.backgroundColor = [UIColor redColor];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
